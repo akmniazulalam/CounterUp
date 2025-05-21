e5d23b5a6c3ecdb01a845554f2f815149e7d1aeb
niazulalam <niazulalam097@gmail.com>
2025-05-22 00:16:37 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let heading = document.querySelectorAll("h1");
+let arr = Array.from(heading);
+
+arr.map((item) => {
+  let count = 0;
+
+  function counterJs() {
+    let increment = item.dataset.number/10
+    count += increment;
+    item.innerHTML = `${count}+`;
+
+    if (count == item.dataset.number) {
+      clearInterval(stop);
+    }
+  }
+
+  let stop = setInterval(() => {
+    counterJs();
+  }, 100);
+});
